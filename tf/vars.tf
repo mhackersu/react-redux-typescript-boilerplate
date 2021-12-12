7a639c87cca9cf52841b3aca80f6522716943422
Mike Hacker <mhackersu@gmail.com>
2021-12-12 11:55:33 -0600

    updating vars

--- tf/vars.tf
+++ tf/vars.tf
@@ -1,6 +1,10 @@
 variable "aws_access_key" {
-  description = "aws"
+  type        = string
+  default     = "aws-access-key"
+  description = "AWS IAM Access Key"
 }
 variable "aws_secret_key" {
-  description = "aws"
+  type        = string
+  default     = "aws-secret-key"
+  description = "AWS IAM Secret Key"
 }
